b103f4d0b5cbb8c4defa9018e69c457ec9069ab7
Pandega <pandegarn@gmail.com>
2025-05-24 22:00:04 +0700

    fix deploy

--- src/app/page.js
+++ src/app/page.js
@@ -1,4 +1,5 @@
 import SectionSumbagan from "@/components/home/SectionSumbangan";
+import prisma from "@/lib/prisma";
 
 export default async function Home() {
   const sumbangan = await prisma.sumbangan.findMany();
