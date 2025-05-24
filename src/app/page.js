ff1d54be24d0154ad3173c628d7b9b2233767134
Pandega <pandegarn@gmail.com>
2025-05-24 21:58:18 +0700

    coba fix in lagi

--- src/app/page.js
+++ src/app/page.js
@@ -1,14 +1,10 @@
-import Hero from "@/components/home/Hero";
 import SectionSumbagan from "@/components/home/SectionSumbangan";
-import { getCurrentUser } from "@/lib/auth/getCurrentUser";
 
 export default async function Home() {
-  const user = await getCurrentUser();
   const sumbangan = await prisma.sumbangan.findMany();
 
   return (
     <div className="w-full">
-      <Hero user={user} />
       <SectionSumbagan
         sumbangan={sumbangan.slice(0, 3)}
         title="Program Donasi Kami"
--- src/components/Buttons.jsx
+++ src/components/Buttons.jsx
@@ -8,23 +8,6 @@ export default function LoginButton({ user }) {
   return (
     <div className="flex items-center justify-between gap-2">
       {user ? (
-        <Link href={"/account"} className="flex items-center gap-2 h-full">
-          <span className="text-end hidden lg:block">
-            <p>{user.name}</p>
-
-            <p>{user.email}</p>
-          </span>
-
-          <Image
-            src={user.image}
-            width={40}
-            height={40}
-            alt="image"
-            className="h-full rounded-lg "
-          />
-        </Link>
-      ) : null}
-      {user ? (
         <button
           onClick={() => signOut()}
           className="px-4 py-2 bg-green-100  text-green-900 rounded-lg hover:bg-green-700 hover:text-green-100 border border-green-100 transition"
